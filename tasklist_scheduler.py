7624dc0a09c9dfae77ea1ee53a8315bca2719a8c
Konrad Drees <konrad.drees@posteo.de>
2022-04-15 11:36:29 +0200

    List Scheduler

---
+++ tasklist_scheduler.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+
+
+task_data = pd.read_csv('t-1-4.txt', delim_whitespace=True)
+
+
+# calculates the max duration in an even distributed scheduler
+def even_list_scheduler(dataframe, number_tasks, ressource):
+    if dataframe.shape[0] < number_tasks:
+        number_tasks = dataframe.shape[0]
+    res_list = np.zeros(ressource)
+    for task in range(number_tasks):
+        current_task = dataframe.loc[task]
+        res_list[task % ressource] += current_task['Runtime']
+    return res_list.max()
+
+
+# calculates the max duration in an optimized distributed scheduler
+def list_scheduler(dataframe, number_tasks, ressource):
+    if dataframe.shape[0] < number_tasks:
+        number_tasks = dataframe.shape[0]
+    res_list = np.zeros(ressource)
+    for task in range(number_tasks):
+        current_task = dataframe.loc[task]
+        min_nr = np.amin(res_list)
+        for i in range(ressource):
+            if res_list[i] == min_nr:
+                res_list[i] += current_task['Runtime']
+                break
+    return res_list.max()
+
+
+print(list_scheduler(task_data, 1000, 2))
+print(list_scheduler(task_data, 1000, 4))
+print(list_scheduler(task_data, 1000, 8))
+print(list_scheduler(task_data, 1000, 16))
+print(even_list_scheduler(task_data, 1000, 2))
+print(even_list_scheduler(task_data, 1000, 4))
+print(even_list_scheduler(task_data, 1000, 8))
+print(even_list_scheduler(task_data, 1000, 16))
+
+data = {'Name': ['imgread', 'grayscale left', 'grayscale right', 'sobel left', 'sobel right', 'SDE', 'imgwrite'], 'Runtime': [1, 2, 2, 6, 6, 10, 1]}
+image_processing_tasks = pd.DataFrame(data)
+print(list_scheduler(image_processing_tasks, 7, 4))
+
